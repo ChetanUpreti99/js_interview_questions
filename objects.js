25d697c0939e1b0f27767ff01f3a2d833e0d301b
Chetan Upreti <chetanupreti@Chetans-MacBook-Air-2.local>
2023-05-11 14:45:16 +0530

    objects output based

--- objects.js
+++ objects.js
@@ -125,3 +125,151 @@ console.log(shape.perimeter()); //NaN
 
 //destructuring of an objects?
 
+const user_3 = {
+    name: 'Chetan',
+    age: 25,
+    fullName: {
+        first: 'Chetan',
+        last: 'Upreti'
+    },
+}
+
+const { name: myName } = user_3;
+console.log(myName);
+
+
+const { fullName: { first, last } } = user_3;
+console.log(`first`, first, `last`, first);
+
+
+//output
+
+// A rest parameter must be last in a parameter list. 
+function getItems(fruitLists, favFruit, ...args) {
+    return [...fruitLists, ...args, favFruit];
+
+}
+
+console.log(getItems(['banana', 'apple'], 'mango', 'kivi'));
+
+
+//output
+let c_1 = { name: 'chetan' };
+let d_1 = c_1; //same reference
+
+c_1.name = 'hye';
+console.log(d_1.name) //hye
+
+
+console.log({ a: 1 } == { a: 1 });
+console.log({ a: 1 } === { a: 1 });
+
+//both object point diff memory so both are false. object are only equal when then both reference the same memory
+
+//output
+
+let person = { name: 'chetan' };
+const members = [person];
+person = null;
+
+console.log(members);
+
+/*
+[
+    {
+        "name": "chetan"
+    }
+] */
+
+//person.name = null if we change variable property then it will change
+
+/* 
+[
+    {
+        "name": null
+    }
+] */
+
+
+const value_1 = { number: 10 };
+//using spread operator object was clone not referenced.
+const multiply = (x = { ...value_1 }) => {
+    console.log((x.number *= 2));
+};
+
+multiply();
+multiply();
+multiply(value_1);
+multiply(value_1);
+
+//output
+
+function changeAgeAndReference(person) {
+    //same reference change here
+    person.age = 35;
+    //reassign variable so new memory will create for object.
+    person = {
+        name: "JHON",
+        age: 50
+    }
+    return person;
+}
+
+const personObj_1 = {
+    name: "chetan",
+    age: 45
+}
+
+const personObj_2 = changeAgeAndReference(personObj_1);
+
+console.log(personObj_1);
+/* 
+{
+    "name": "chetan",
+    "age": 35
+}
+ */
+console.log(personObj_2);
+/*
+{
+    name: "JHON",
+    age: 50
+}
+*/
+
+//shallow copy and Deep copy or clone copy
+
+//shallow copy - when we copy one object to another then some of property has same reference
+//Deep copy or clone copy - another object has no reference for 1st object where it made
+
+let user_4 = {
+    name: "chetan",
+    age: 23,
+    fullName: {
+        first: 'chetan',
+        last: "upreti"
+    }
+}
+
+//1. NOT DEEP
+
+// let objClone_1 = Object.assign({}, user_4); //will not clone nested objects
+// objClone_1.name = 'upreti'
+// objClone_1.age = 45;
+// objClone_1.fullName.first = 'ram'
+// console.log(user_4, objClone_1);
+
+//2. DEEP
+// let objClone_2 = JSON.parse(JSON.stringify(user_4));
+// objClone_2.name = 'upreti'
+// objClone_2.age = 45;
+// objClone_2.fullName.first = 'ram'
+// console.log(user_4, objClone_2);
+
+//3. NOT DEEP
+let objClone_3 = { ...user_4 };
+objClone_3.name = 'upreti'
+objClone_3.age = 45;
+objClone_3.fullName.first = 'ram'
+console.log(user_4, objClone_3);
+
