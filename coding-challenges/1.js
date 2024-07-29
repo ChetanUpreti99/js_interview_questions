87627ca19abad6dfa9cad3f4dcd85ad7ec38eb80
ChetanUpreti99 <chetanupreti97@gmail.com>
2024-07-29 18:49:56 +0530

    feat: 1.js

--- coding-challenges/1.js
+++ coding-challenges/1.js
@@ -74,3 +74,47 @@ let output = obj.reduce((acc, { data, key }) => {
 }
  */
 console.log(output);
+
+const add = (a, b) => a + b;
+
+function memoizeOne(fn) {
+    let result = {};
+    return function (a, b) {
+        if (result[`${a}${b}`]) {
+            console.log(`here`, result[`${a}${b}`]);
+            return result[`${a}${b}`];
+        } else {
+            result[`${a}${b}`] = fn(a, b);
+            return result[`${a}${b}`];
+        }
+    }
+
+}
+
+const memoizeAdd = memoizeOne(add);
+
+console.log(memoizeAdd(1, 3));
+
+console.log(memoizeAdd(1, 3));
+console.log(memoizeAdd(1, 5));
+console.log(memoizeAdd(1, 5));
+
+
+
+const arr = [1, 2, 3, 4, [5, [6, 7]], 8, 9, 10];
+
+function flattenArr(arr) {
+    let result = [];
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (Array.isArray(element)) {
+            result.push(...flattenArr(element));
+        } else {
+            result.push(element);
+        }
+
+    }
+    return result;
+}
+
+console.log(flattenArr(arr))
